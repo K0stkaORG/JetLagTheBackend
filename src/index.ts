f0055a3a51e3bdfb7fecea3a9f5101af10752e8e
K0stka <www.honza.90@gmail.com>
2025-06-06 01:34:36 +0200

    Fix cors

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import { handleLoginAttempt, handleRegistrationAttempt } from "./handlers/auth";
 
 import { Server } from "socket.io";
+import cors from "cors";
 import { env } from "./env";
 import express from "express";
 
 const REST_API = express();
 REST_API.use(express.json());
+REST_API.use(cors());
 
 const WS_SERVER = new Server({
 	cors: {
