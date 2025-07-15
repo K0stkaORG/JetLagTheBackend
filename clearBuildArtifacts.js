ecca2811ab4bcb00d5855242a6763cc20a56592e
K0stka <91200807+K0stka@users.noreply.github.com>
2025-07-15 17:04:47 +0200

    Build step final touches

--- clearBuildArtifacts.js
+++ clearBuildArtifacts.js
@@ -1,9 +1,13 @@
 var fs = require("fs");
 
+console.log("Clearing build artifacts...");
+
 try {
 	fs.rmdirSync("./dist", { recursive: true });
 } catch (e) {
-	console.warn("Error when removing folder: ./dist\n", e);
+	console.warn("Error when removing folder: ./dist: ", e.message);
 }
+
+console.log("Building application...");
 
 process.exit(0);
